fbde473efed3886fb06ca46180e11a787247a347
Haider Ali <88369234+HaiderAli785@users.noreply.github.com>
2022-08-03 20:33:34 +0500

    Update question10.java

--- Code/Practice Java Questions(After Lab Task 1)/question10.java
+++ Code/Practice Java Questions(After Lab Task 1)/question10.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
-public class question10 {
-    public static void main(String args[]){
+public class question10 
+{
+    
+    public static void main(String args[])
+    {
 
         Scanner sc = new Scanner(System.in);
 
@@ -10,9 +13,15 @@ public class question10 {
         String input = sc.next();
 
         string(input);        
+        
     }
-    public static void string(String input){
+    
+    public static void string(String input)
+    {
+        
         System.out.println("The length of string is: " + input.length());
+        
         System.out.println("The first character of the string is: " + input.charAt(0));
+        
     }
 }
